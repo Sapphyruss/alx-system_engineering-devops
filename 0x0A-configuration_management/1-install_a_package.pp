54d4f809c01932162a91d0e28b12bb01b43dd7da
Sapphyruss <lugi.franklin@outlook.fr>
2023-11-24 13:09:57 -0800

    .

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,11 @@
-class { 'python':
-  version => 'system',
-}
+# 1-install_a_package.pp
 
 package { 'python3-pip':
   ensure => present,
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin:/bin',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
-}
-
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Exec['install_flask'],
+  require  => Package['python3-pip'],
 }
