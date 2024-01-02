9bc44238b2caf8f5cfbb48e405ad0374a502c935
Sapphyruss <lugi.franklin@outlook.fr>
2024-01-02 13:56:37 -0800

    .

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
-"""Retrieve TODO list information for a given employee ID."""
-import requests
-import sys
+"""This script retrieves and displays a user's TODO list from a REST API."""
+from requests import get
+from sys import argv
+
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    employee_ID = argv[1]
+    api = "https://jsonplaceholder.typicode.com/"
+    user_info = f"users/{employee_ID}"
+    to_do_list = f"todos?userId={employee_ID}"
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    # Retrieve user information
+    response = get(api + user_info)
+    employee_info = response.json()
+    employee_name = f"Employee {employee_info['name']}"
+    padded_employee_name = employee_name.ljust(18)  # Ensure 18 characters length
+
+    # Retrieve tasks information
+    response = get(api + to_do_list)
+    tasks = response.json()
+    
+    completed_tasks = [task for task in tasks if task['completed']]
+    num_completed_tasks = len(completed_tasks)
+    total_tasks = len(tasks)
+
+    # Display information
+    print(f"{padded_employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
+
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
+
