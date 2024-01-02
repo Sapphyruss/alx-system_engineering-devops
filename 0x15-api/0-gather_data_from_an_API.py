18784aa8ab97a4909d8edab8e3a251f250699ac2
Sapphyruss <lugi.franklin@outlook.fr>
2024-01-02 13:46:44 -0800

    .

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Retrieve and displays a user's todo list from a REST API."""
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    employee_ID = argv[1]
+    api = f"https://jsonplaceholder.typicode.com/"
+    user_info = f"users/{employee_ID}"
+    to_do_list = f"todos?userId={employee_ID}"
+
+    response = get(api + user_info)
+    employee_info = response.json()
+    response = get(api + to_do_list)
+    tasks = response.json()
+
+    employee_name = f"Employee {employee_info['name']}"
+    completed_task = [task for task in tasks if task['completed']]
+    no_comp_task = len(completed_task)
+    no_tasks = len(tasks)
+
+    print(f"{employee_name} is done with tasks({no_comp_task}/{no_tasks}):")
+
+    for task in completed_task:
+        print(f"\t {task['title']}")
