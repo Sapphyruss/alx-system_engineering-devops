ef46c8dc42ab8ecef3ddbaf4b7e2331b29ab755d
Sapphyruss <lugi.franklin@outlook.fr>
2024-01-16 10:11:51 -0800

    .

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Query the Reddit API and returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """queries the Reddit API and returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = {'User-agent': 'ALX project'}
+    response = requests.get(url, headers=header, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+
+    data = response.json()
+    return data['data']["subscribers"]
