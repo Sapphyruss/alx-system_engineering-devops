5a5308312543285f8136542ef6b89ef160caa1ce
Sapphyruss <lugi.franklin@outlook.fr>
2024-01-18 05:13:53 -0800

    .

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-exec {'fix500':
-  command => 'sed -i "s|require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );\
-|require_once( ABSPATH . WPINC . '/class-wp-locale.php' );|g" /var/www/html/wp-settings.php',
-  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/'
+# Fix phpp extension
+
+exec { 'fix-500':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
